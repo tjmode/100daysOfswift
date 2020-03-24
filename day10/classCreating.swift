5c90111032f305019a11438c1ecd26e67a5fb810
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-24 19:58:09 +0530

    Add files via upload

---
+++ day10/classCreating.swift
@@ -0,0 +1,10 @@
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+let poppy = Dog(name: "Poppy", breed: "Poodle")
---
+++ day10/finalCLass.swift
@@ -0,0 +1,10 @@
+final class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+// when you declare a class as being final
