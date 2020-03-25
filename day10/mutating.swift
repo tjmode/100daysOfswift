07b9562a7f45f00302550a9fa5ccdd5c4c25f7fb
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-25 19:07:50 +0530

    Add files via upload

---
+++ day10/mutating.swift
@@ -0,0 +1,12 @@
+class Singer {
+    var name = "Taylor Swift"
+}
+
+let taylor = Singer()
+taylor.name = "Ed Sheeran"
+print(taylor.name)
+
+class Singer {
+    let name = "Taylor Swift"
+}
+// mutating 
